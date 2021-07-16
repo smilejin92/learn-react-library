d62e6067dd0eea12eb43a1b02bee6307a3a2acc4
smilejin92 <smilejin92@gmail.com>
2021-07-16 13:53:53 +0900

    [feat] 테스트 용도에 맞게 jsx 마크업 수정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,23 @@
-import React from 'react'
-import "./Header.css"
+import React from 'react';
+import './Header.css';
 
-export default function Header({
-    title
-}) {
-    return <h1 className="header">{title}</h1>
+export default function Header({ title }) {
+  // 테스트 용
+  return (
+    <>
+      <h1 className="header" data-testid="header-1">
+        {title}
+      </h1>
+      <h3 className="header" title="Header">
+        Cats
+      </h3>
+    </>
+  );
+
+  // 원본
+  //   return (
+  //       <h1 className="header">
+  //         {title}
+  //       </h1>
+  //   );
 }

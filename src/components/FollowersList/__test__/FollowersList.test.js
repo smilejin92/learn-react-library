9467ce0a7b5af22270145b11b11740e7b3e5197c
smilejin92 <smilejin92@gmail.com>
2021-07-16 19:43:23 +0900

    [test] FollowersList 테스트 코드 수정
    - before/afterEach, before/afterAll 테스트

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -9,6 +9,20 @@ const MockFollowersList = () => (
 );
 
 describe('FollowersList', () => {
+  /**
+   * https://jestjs.io/docs/api#afterallfn-timeout
+   * hook: beforeEach, afterEach, beforeAll, afterAll
+   * 이곳에 사용된 hook은 이 describe 블록 안에서만 적용됨
+   */
+
+  beforeEach(() => {
+    console.log('before each test');
+  });
+
+  beforeAll(() => {
+    console.log('before all tests in this test block');
+  });
+
   test('1. 팔로워 목록 중 첫 번째 아이템이 표시된다.', async () => {
     render(<MockFollowersList />);
     const followerDivElem = await screen.findByTestId('follower-item-0');
@@ -19,5 +33,13 @@ describe('FollowersList', () => {
     render(<MockFollowersList />);
     const followerDivElems = await screen.findAllByTestId(/follower-item/i);
     expect(followerDivElems.length).toBe(5);
+  });
+
+  afterEach(() => {
+    console.log('after each test');
+  });
+
+  afterAll(() => {
+    console.log('after all tests in this test block');
   });
 });

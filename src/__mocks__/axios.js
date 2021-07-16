f998393943a2a1f2fbc6ee269f9e710b2d58ad39
smilejin92 <smilejin92@gmail.com>
2021-07-16 19:23:12 +0900

    [feat] mock module 생성 - axios
    - 테스트 사용 할 mock axios 모듈 생성

---
+++ src/__mocks__/axios.js
@@ -0,0 +1,303 @@
+const mockData = {
+  results: [
+    {
+      gender: 'male',
+      name: {
+        title: 'Mr',
+        first: 'Jin',
+        last: 'Kim',
+      },
+      location: {
+        street: {
+          number: 981,
+          name: 'Simcoe St',
+        },
+        city: 'Burlington',
+        state: 'Nunavut',
+        country: 'Canada',
+        postcode: 'M2L 0C8',
+        coordinates: {
+          latitude: '-62.5614',
+          longitude: '-110.6408',
+        },
+        timezone: {
+          offset: '+10:00',
+          description: 'Eastern Australia, Guam, Vladivostok',
+        },
+      },
+      email: 'nicolas.ma@example.com',
+      login: {
+        uuid: 'b9f1d19f-87ac-4b93-a737-0089df7b8741',
+        username: 'blueladybug148',
+        password: 'jamaica',
+        salt: 'IEPWSMny',
+        md5: 'c0cccf610803ce65b0f0da5d2e85c7c2',
+        sha1: '219d9f24324eb85cb0a070d27bd18739d43b5485',
+        sha256:
+          'e295c7b7ef5ae4d87f6a8c10091bf9edfe8964b9271a11a306381020126246a8',
+      },
+      dob: {
+        date: '1945-11-16T02:12:31.232Z',
+        age: 76,
+      },
+      registered: {
+        date: '2011-11-25T03:05:46.459Z',
+        age: 10,
+      },
+      phone: '894-654-3040',
+      cell: '982-731-4886',
+      id: {
+        name: '',
+        value: null,
+      },
+      picture: {
+        large: 'https://randomuser.me/api/portraits/men/56.jpg',
+        medium: 'https://randomuser.me/api/portraits/med/men/56.jpg',
+        thumbnail: 'https://randomuser.me/api/portraits/thumb/men/56.jpg',
+      },
+      nat: 'CA',
+    },
+    {
+      gender: 'male',
+      name: {
+        title: 'Mr',
+        first: 'Eduardo',
+        last: 'Woods',
+      },
+      location: {
+        street: {
+          number: 5061,
+          name: 'Woodland St',
+        },
+        city: 'Honolulu',
+        state: 'Arizona',
+        country: 'United States',
+        postcode: 62117,
+        coordinates: {
+          latitude: '-10.3909',
+          longitude: '-153.8318',
+        },
+        timezone: {
+          offset: '+3:30',
+          description: 'Tehran',
+        },
+      },
+      email: 'eduardo.woods@example.com',
+      login: {
+        uuid: 'fe3b427d-d974-4701-8f14-f98f609ed746',
+        username: 'organicladybug692',
+        password: 'hotshot',
+        salt: '52EqxyrR',
+        md5: 'ce2415b62462f948b77d557104995253',
+        sha1: 'cabcf974bf05612abab2e5d61ba2a6cd35e110a9',
+        sha256:
+          '74a49909054df4480f5488bf6ec6c0d32ddf50fd61acc16af4daaa20e0e92a0d',
+      },
+      dob: {
+        date: '1961-12-26T06:14:43.255Z',
+        age: 60,
+      },
+      registered: {
+        date: '2015-12-25T00:08:09.915Z',
+        age: 6,
+      },
+      phone: '(947)-975-8886',
+      cell: '(469)-970-3215',
+      id: {
+        name: 'SSN',
+        value: '989-90-0511',
+      },
+      picture: {
+        large: 'https://randomuser.me/api/portraits/men/90.jpg',
+        medium: 'https://randomuser.me/api/portraits/med/men/90.jpg',
+        thumbnail: 'https://randomuser.me/api/portraits/thumb/men/90.jpg',
+      },
+      nat: 'US',
+    },
+    {
+      gender: 'female',
+      name: {
+        title: 'Mrs',
+        first: 'Mery',
+        last: 'Cavalcanti',
+      },
+      location: {
+        street: {
+          number: 3152,
+          name: 'Rua Pernambuco ',
+        },
+        city: 'Itu',
+        state: 'Amapá',
+        country: 'Brazil',
+        postcode: 82364,
+        coordinates: {
+          latitude: '-17.9029',
+          longitude: '-76.0528',
+        },
+        timezone: {
+          offset: '-8:00',
+          description: 'Pacific Time (US & Canada)',
+        },
+      },
+      email: 'mery.cavalcanti@example.com',
+      login: {
+        uuid: 'b797a5ce-f7cf-4c8e-9ea6-9ffb06428fce',
+        username: 'silverfish435',
+        password: 'hogan',
+        salt: 'EO44qaas',
+        md5: 'cabfa6d3507e8f17d44883488ff80a8b',
+        sha1: '834d81c217e32f9d5eae5ff94e5764ed21c6bb61',
+        sha256:
+          'a3b900356bb6a6e9082048761dc4562b3f5a4261207baa95312a47d372b0b149',
+      },
+      dob: {
+        date: '1973-08-14T06:58:00.281Z',
+        age: 48,
+      },
+      registered: {
+        date: '2009-07-05T01:34:45.541Z',
+        age: 12,
+      },
+      phone: '(91) 4966-1618',
+      cell: '(34) 0121-5454',
+      id: {
+        name: '',
+        value: null,
+      },
+      picture: {
+        large: 'https://randomuser.me/api/portraits/women/51.jpg',
+        medium: 'https://randomuser.me/api/portraits/med/women/51.jpg',
+        thumbnail: 'https://randomuser.me/api/portraits/thumb/women/51.jpg',
+      },
+      nat: 'BR',
+    },
+    {
+      gender: 'male',
+      name: {
+        title: 'Mr',
+        first: 'Léandro',
+        last: 'Laurent',
+      },
+      location: {
+        street: {
+          number: 2447,
+          name: "Place de L'Abbé-Georges-Hénocque",
+        },
+        city: 'Orléans',
+        state: 'Marne',
+        country: 'France',
+        postcode: 56808,
+        coordinates: {
+          latitude: '86.7600',
+          longitude: '-177.3808',
+        },
+        timezone: {
+          offset: '-11:00',
+          description: 'Midway Island, Samoa',
+        },
+      },
+      email: 'leandro.laurent@example.com',
+      login: {
+        uuid: '18221ca2-8257-461c-baee-40047b45868e',
+        username: 'blackkoala920',
+        password: 'lawman',
+        salt: 'RxihgloI',
+        md5: 'f008ddbd56845ed9e2cae84ea870f174',
+        sha1: '14641fde974eb4d2e9b6a0a5584dfaa7d0caa4d8',
+        sha256:
+          'da8f9d602631c0a1f940b31e2ed6ef427a0055aaf5db5653fa9153e9aa6158df',
+      },
+      dob: {
+        date: '1957-04-08T22:11:31.112Z',
+        age: 64,
+      },
+      registered: {
+        date: '2006-05-15T02:37:31.646Z',
+        age: 15,
+      },
+      phone: '05-76-34-79-93',
+      cell: '06-03-43-34-13',
+      id: {
+        name: 'INSEE',
+        value: '1NNaN28430837 26',
+      },
+      picture: {
+        large: 'https://randomuser.me/api/portraits/men/98.jpg',
+        medium: 'https://randomuser.me/api/portraits/med/men/98.jpg',
+        thumbnail: 'https://randomuser.me/api/portraits/thumb/men/98.jpg',
+      },
+      nat: 'FR',
+    },
+    {
+      gender: 'male',
+      name: {
+        title: 'Mr',
+        first: 'Belisário',
+        last: 'Nascimento',
+      },
+      location: {
+        street: {
+          number: 7015,
+          name: 'Rua São José ',
+        },
+        city: 'Simões Filho',
+        state: 'Tocantins',
+        country: 'Brazil',
+        postcode: 69479,
+        coordinates: {
+          latitude: '-62.9274',
+          longitude: '-5.5245',
+        },
+        timezone: {
+          offset: '+8:00',
+          description: 'Beijing, Perth, Singapore, Hong Kong',
+        },
+      },
+      email: 'belisario.nascimento@example.com',
+      login: {
+        uuid: 'faa2a1e5-7023-4187-8677-87033aa8b621',
+        username: 'sadduck260',
+        password: 'sirius',
+        salt: 'tF7iys8p',
+        md5: 'ae05ffe5889abacaa406276af01591da',
+        sha1: 'e499bebd3073177b6694a974afb5c588fc523cb4',
+        sha256:
+          '0664d795a98f263a59eefac41d0f12c8098095ed85df4cfcf7226847a97295d2',
+      },
+      dob: {
+        date: '1975-03-29T03:50:18.251Z',
+        age: 46,
+      },
+      registered: {
+        date: '2006-12-14T23:32:52.139Z',
+        age: 15,
+      },
+      phone: '(96) 5737-4823',
+      cell: '(02) 6285-4057',
+      id: {
+        name: '',
+        value: null,
+      },
+      picture: {
+        large: 'https://randomuser.me/api/portraits/men/69.jpg',
+        medium: 'https://randomuser.me/api/portraits/med/men/69.jpg',
+        thumbnail: 'https://randomuser.me/api/portraits/thumb/men/69.jpg',
+      },
+      nat: 'BR',
+    },
+  ],
+  info: {
+    seed: 'f479920431194a11',
+    results: 5,
+    page: 1,
+    version: '1.3',
+  },
+};
+
+const mockAxios = {
+  get: jest.fn().mockResolvedValue({
+    data: mockData,
+  }),
+};
+
+export default mockAxios;
